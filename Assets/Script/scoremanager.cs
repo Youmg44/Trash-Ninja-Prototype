e524c6258ea7ca5d0dcdc238062f514994e8b3fe
INotAntony <antonio.garcia@student.saedubai.com>
2022-02-18 17:17:01 +1100

    added score manager (accidently removed sprite models)

---
+++ Assets/Script/scoremanager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoremanager : MonoBehaviour
+{
+    public static scoremanager instance;
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        scoreText.text = score.ToString() + " POINTS";
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void AddPoint()
+    {
+        score += 1;
+        scoreText.text = score.ToString() + " POINTS";
+    }
+}
